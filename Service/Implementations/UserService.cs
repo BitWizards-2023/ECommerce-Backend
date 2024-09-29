24407f9b762708580d6d9e0900c09f76b199d84b
Sachintha <sudesh@orelit.com>
2024-09-29 13:12:08 +0530

    add the comments

--- Service/Implementations/UserService.cs
+++ Service/Implementations/UserService.cs
@@ -1,6 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+/*
+ * Author: Sudesh Sachintha Bandara
+ * Description: This file contains the implementation of the UserService class,
+ * which provides functionality to manage and retrieve user information from the database.
+ * Date Created: 2024/09/28
+ */
+
 using ECommerceBackend.Data.Contexts;
 using ECommerceBackend.Data.Repository.Interfaces;
 using ECommerceBackend.DTOs.Response;
@@ -8,22 +12,26 @@ using ECommerceBackend.Helpers;
 using ECommerceBackend.Models;
 using MongoDB.Driver;
 
-namespace ECommerceBackend.Data.Repository.Implementations;
-
-public class UserService : IUserServices
+namespace ECommerceBackend.Data.Repository.Implementations
 {
-    private readonly MongoDbContext _context;
-    private readonly IConfiguration _configuration;
-
-    public UserService(MongoDbContext context, IConfiguration configuration)
+    public class UserService : IUserServices
     {
-        _context = context ?? throw new ArgumentNullException(nameof(context));
-        _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
-    }
+        private readonly MongoDbContext _context;
+        private readonly IConfiguration _configuration;
 
-    public List<UserResponseDTO> GetUserList()
-    {
-        var users = _context.Users.Find(FilterDefinition<User>.Empty).ToList();
-        return users.Select(DtoMapper.ToUserResponseDTO).ToList();
+        // Constructor for the UserService class, initializes the database context and configuration
+        public UserService(MongoDbContext context, IConfiguration configuration)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _configuration =
+                configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        // Retrieves a list of all users from the database and maps them to UserResponseDTO objects
+        public List<UserResponseDTO> GetUserList()
+        {
+            var users = _context.Users.Find(FilterDefinition<User>.Empty).ToList();
+            return users.Select(DtoMapper.ToUserResponseDTO).ToList();
+        }
     }
 }
